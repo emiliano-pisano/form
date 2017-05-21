88652cc54d8f691f28421d4e9c55d223fa47009d
emiliano-pisano <pisanoemiliano@gmail.com>
2017-05-21 18:07:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,61 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+$(function () {
+    'use strict';
 
-	    var target = this.hash;
-	    var $target = $(target);
+    $("li a[href^='#']").on('click', function(e) {
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // store hash
+        var hash = this.hash;
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1100, function(){
+
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+    });
+
+    $('form').bootstrapValidator({
+        fields: {
+            nome: {
+                validators: {
+                    stringLength: {
+                        min: 3,
+                        message: 'Per favore inserisci piu di 3 caratteri'
+                        
+                    },
+                    notEmpty: {
+                        message: 'Per favore inserisci il tuo nome'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Per favore inserisci il tuo indirizzo email'
+                    },
+                    emailAddress: {
+                        message: 'Per favore inserisci un indirizzo email valido'
+                    }
+                }
+            },
+            telefono: {
+                validators: {
+                    notEmpty: {
+                        message: 'Per favore inserisci il tuo numero di telefono'
+                    },
+                    phone: {
+                        country: 'US',
+                        message: 'Per favore inserisci il tuo numero di telefono con relativo prefisso'
+                    }
+                }
+            }
+        }
+    });
+
 });
