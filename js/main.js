2fc4ecc34edf76a877156c966ca54c43f7802e41
emiliano-pisano <pisanoemiliano@gmail.com>
2017-05-25 13:33:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    
     'use strict';
 
     $("li a[href^='#']").on('click', function(e) {
@@ -11,45 +12,65 @@ $(function () {
 
         // animate
         $('html, body').animate({
+            
             scrollTop: $(hash).offset().top
+            
         }, 1100, function(){
 
             // when done, add hash to url
             // (default click behaviour)
             window.location.hash = hash;
+            
         });
     });
 
     $('form').bootstrapValidator({
+        
         fields: {
+            
             nome: {
+                
                 validators: {
+                    
                     stringLength: {
+                        
                         min: 3,
                         message: 'Per favore inserisci piu di 3 caratteri'
                         
                     },
+                    
                     notEmpty: {
+                        
                         message: 'Per favore inserisci il tuo nome'
                     }
                 }
             },
             email: {
+                
                 validators: {
+                    
                     notEmpty: {
+                        
                         message: 'Per favore inserisci il tuo indirizzo email'
                     },
+                    
                     emailAddress: {
+                        
                         message: 'Per favore inserisci un indirizzo email valido'
                     }
                 }
             },
             telefono: {
+                
                 validators: {
+                    
                     notEmpty: {
+                        
                         message: 'Per favore inserisci il tuo numero di telefono'
+                        
                     },
                     phone: {
+                        
                         country: 'US',
                         message: 'Per favore inserisci il tuo numero di telefono con relativo prefisso'
                     }
